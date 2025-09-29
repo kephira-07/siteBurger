069b3f0481bc89a76846c73e2635b9f0929c3cfc
Kephira <arielleahlonkoba@gmail.com>
2025-09-29 15:00:35 +0000

    modifier

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+ const navbar =document.querySelector("nav");
+   let valeurlast = 0;
+  window.addEventListener("scroll",()=>{
+       if (window.scrollY > valeurlast){
+        navbar.style.top="-100px";
+       }
+       else{
+         navbar.style.top = 0;
+       }
+       valeurlast = window.scrollY;
+  })
  function slowSpeed() {
             const scroll = document.querySelector('.gallery-scroll');
             scroll.style.animationDuration = '30s';
